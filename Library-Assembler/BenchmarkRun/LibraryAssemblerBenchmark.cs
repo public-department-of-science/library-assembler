d0998894eca40d9fceb395baa942718493e0f037
bohdan skrypka <skripkabodya@gmail.com>
2022-05-16 21:17:54 +0300

    - add BenchmarkRun.csproj to have things to measure as inside

---
+++ Library-Assembler/BenchmarkRun/LibraryAssemblerBenchmark.cs
@@ -0,0 +1,30 @@
+﻿using BenchmarkDotNet.Attributes;
+using Library.Assembler;
+using System.IO;
+using System.Text;
+
+namespace Common.Benchmarking
+{
+    [MemoryDiagnoser]
+    [KeepBenchmarkFiles]
+    public class LibraryAssemblerBenchmark
+    {
+        string stringValue;
+
+        //    [Params(@"G:\DeskTop\grammar-analyzer\GrammarAnalyzer\04.Samples.CodePlace\BenchmarkReadingSamples\sample-text-file.txt",
+        //@"G:\DeskTop\grammar-analyzer\GrammarAnalyzer\04.Samples.CodePlace\BenchmarkReadingSamples\sample-2mb-text-file.txt")]
+        //    public string pathToTheFile;
+
+        //[GlobalSetup]
+        //public void ReadFileAsString()
+        //{
+        //    stringValue = File.ReadAllText(pathToTheFile);
+        //}
+
+        [Benchmark]
+        public void StringBuilderFixedSize()
+        {
+            Program.Main();
+        }
+    }
+}
---
+++ Library-Assembler/BenchmarkRun/Program.cs
@@ -0,0 +1,12 @@
+﻿using BenchmarkDotNet.Running;
+using Common.Benchmarking;
+
+public static class BenchmarkRun
+{
+    public static void Main()
+    {
+        //var summary = BenchmarkRunner.Run<FIleReadOperations>();
+        var summary1 = BenchmarkRunner.Run<LibraryAssemblerBenchmark>();
+        //var summary2 = BenchmarkRunner.Run<StringBulderBenchmark>();
+    }
+}
